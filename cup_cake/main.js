582e209597418b8ece94c9d661e06475470a1427
medaliabid <dali.abid2014@gmail.com>
2023-05-09 01:52:07 +0200

    done

--- cup_cake/main.js
+++ cup_cake/main.js
@@ -16,25 +16,32 @@ var array= ["https://cdn.cnn.com/cnnnext/dam/assets/150929101049-black-coffee-st
 "https://t4.ftcdn.net/jpg/01/16/61/93/360_F_116619399_YA611bKNOW35ffK0OiyuaOcjAgXgKBui.jpg",
 "https://assets.epicurious.com/photos/57ced55ee6411cab70f0d948/1:1/w_1600,c_limit/fresh-mint-and-ginger-lemonade.jpg",
 "https://jrad-distribution.tn/wp-content/uploads/2020/07/boisson-gazeuse-11.png",
-"https://cdn.w600.comps.canstockphoto.com/back-soon-illustration_csp28677029.jpg"];
+"https://cdn.w600.comps.canstockphoto.com/back-soon-illustration_csp28677029.jpg",
+"https://static.750g.com/images/1200-675/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg",
+"https://static.toiimg.com/thumb/msid-80850219,width-1070,height-580,imgsize-735181,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+"https://www.bhg.com/thmb/iL-5Q6gGjmXkxCKqEovughTLQAo=/3000x0/filters:no_upscale():strip_icc()/how-to-bake-how-to-make-cupcakes-hero-01-12c03f3eff374d569b0565bff7d9e597.jpg",
+"https://gateauetcuisinerachida.com/wp-content/uploads/2015/03/Recette-cheesecake-new-yorkais.jpg"];
 
 
 
 nex.onclick = function () {
     count++ 
     console.log(count)
-    // if(count > array.length) {return count = array.length-1 }
     imge.src = array[Math.abs(count % array.length)]
 }
 
 prev.onclick = function () {
     count--;
     console.log(count)
-    // if(count <= 0) {return count === 0}
     imge.src = array[Math.abs(count% array.length)];
   };
 
 
+
+
+
+
+  
 
   add.onclick = function () {
     var drink = document.getElementById("drink").value
@@ -46,4 +53,6 @@ prev.onclick = function () {
     var list =  elemnts.appendChild(node);
    boody.appendChild(list)
   }
+
+
 
